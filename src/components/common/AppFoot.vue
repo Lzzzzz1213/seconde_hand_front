1df1c74e833e2ebd1a4d0a9e0ecc2ac80cf04df1
like <498684205@qq.com>
2023-04-04 15:40:40 +0800

    Initial commit

---
+++ src/components/common/AppFoot.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="foot-container">
+        <div class="author">基于SpringBoot的二手交易系统，作者：毛奕铭</div>
+        <br>
+        <div class="finishtime">完成时间2023-3-15</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Foot"
+    }
+</script>
+
+<style scoped>
+    .foot-container {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        height: 40px;
+        padding-top: 10px;
+        position: relative;
+        text-align: center;
+    }
+
+    .author {
+        color: #999999;
+        font-size: 14px;
+
+    }
+    .finishtime{
+      color: #999999;
+      font-size: 14px;
+      position: absolute;
+    }
+</style>
